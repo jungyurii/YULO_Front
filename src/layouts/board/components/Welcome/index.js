7dd2e0ca69c2ef80368139b5aa944f1f928147c4
jungyurii <128377892+jungyurii@users.noreply.github.com>
2024-06-03 23:42:27 +0900

    Feat: 공지사항 list 값 받아오기

--- src/layouts/board/components/Welcome/index.js
+++ src/layouts/board/components/Welcome/index.js
@@ -1,63 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "@mui/material";
 
 // Vision UI Dashboard React examples
 import Table from "examples/Tables/Table";
 
-import VuiButton from "components/VuiButton";
+import axios from "axios";
 
-const Welcome = () => {
-  return (
-    <Card>
-      <Table
-        columns={[
-          { name: "id", align: "left" },
-          { name: "name", align: "left" },
-          { name: "title", align: "center" },
-          { name: "date", align: "center" },
-          { name: "detail", align: "center" },
-        ]}
-        rows={[
-          {
-            name: "Manager",
-            title: "John Micheal",
-            date: "23/04/18",
-            detail: <VuiButton color="info">detail</VuiButton>,
-          },
-          {
-            name: "John Micheal",
-            function: "Programator",
-            email: "alexa@user.com",
-            employed: "11/01/19",
-          },
-          {
-            name: "John Micheal",
-            function: "Executive",
-            email: "laurent@user.com",
-            employed: "19/09/17",
-          },
-          {
-            name: "John Micheal",
-            function: "Backend Developer",
-            email: "michael@user.com",
-            employed: "24/12/08",
-          },
-          {
-            name: "John Micheal",
-            function: "Manager",
-            email: "richard@user.com",
-            employed: "04/10/21",
-          },
-          {
-            name: "John Micheal",
-            function: "Programtor",
-            email: "miriam@user.com",
-            employed: "14/09/20",
-          },
-        ]}
-      />
-    </Card>
-  );
-};
+function Welcome() {
+
+  const [noticelist, setNoticelist] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8080/board/listNotice?page=1")
+    .then(response => {
+      console.log("Response : ", response.data.result.data.content);
+      setNoticelist(response.data.result.data.content);
+    })
+    .catch(error => {
+      console.log("Error : ", error);
+    });
+  },[])
+
+  const tableRows = noticelist.map((notice, index) => ({
+    id: notice.noticeId,
+    name: notice.userName,
+    title: notice.title,
+  }));
+
+    return (
+      <Card>
+        <Table
+          columns={[
+            { name: "id", align: "left" },
+            { name: "name", align: "left" },
+            { name: "title", align: "center" },
+            { name: "date", align: "center" },
+          ]}
+          rows={tableRows}
+        />
+      </Card>
+    );
+  };
 
 export default Welcome;
