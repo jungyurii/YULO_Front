b978d8e11b05cacdadf659fa1156d5a40cd53a02
jungyurii <128377892+jungyurii@users.noreply.github.com>
2024-06-05 19:44:00 +0900

    Design: 자유게시판 글 작성 틀

--- src/layouts/board/index.js
+++ src/layouts/board/index.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import React from "react";
 
 // @mui material components
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
-import { Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
-import Pagination from '@mui/material/Pagination';
+import { Dialog, DialogActions, DialogContent, Box, TextField, Typography, createTheme, ThemeProvider, Pagination } from "@mui/material";
+import MUIRichTextEditor from "mui-rte";
 
 // Images
 import profile1 from "assets/images/profile-1.png";
@@ -17,6 +19,7 @@ import team4 from "assets/images/avatar4.png";
 import VuiBox from "components/VuiBox";
 import VuiTypography from "components/VuiTypography";
 import VuiButton from "components/VuiButton";
+import VuiBadge from "components/VuiBadge";
 import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
 
 // Vision UI Dashboard React example components
@@ -27,12 +30,8 @@ import Header from "layouts/board/components/Header";
 import Welcome from "./components/Welcome";
 import Footer from "examples/Footer";
 
-// import Pagination from '@mui/material/Pagination';
-
-import axios from "axios";
-import React from "react";
-import VuiInput from "components/VuiInput";
-
+// React icons
+import { IoChatbubbles } from "react-icons/io5";
 
 function Board() {
   const [view, setView] = useState(true);
@@ -47,7 +46,8 @@ function Board() {
 
   const [boardlist, setBoardlist] = useState([]);
   const [open, setOpen] = useState(false);
-  const [clickBoardIndex, setClickBoardIndex] = useState(); // 클릭된 카드를 식별하기 위한 상태 추가
+
+  const myTheme = createTheme({});
 
   const handleOpen = (boardId) => {
     console.log("boardId : ", boardId);
@@ -77,12 +77,6 @@ function Board() {
     })
     .then(response => {
       console.log("Response : ", response.data.result.data);
-      console.log("TotalPages : ", response.data.result.data.totalPages);
-      console.log("CurrentPage : ", response.data.result.data.number + 1);
-      console.log("TotalElements : ", response.data.result.data.totalElements);
-      console.log("Size : ", response.data.result.data.size);
-      console.log("First : ", response.data.result.data.first);
-      console.log("Last : ", response.data.result.data.last);
       setBoardlist(response.data.result.data.content);
       setTotalPages(response.data.result.data.totalPages); // 전체 페이지 수 설정
       setCurrentPage(response.data.result.data.number + 1); // 현재 페이지 번호 설정
@@ -131,7 +125,7 @@ function Board() {
                           type: "internal",
                           color: "white",
                           label: "VIEW ALL",
-                          onClick: () => handleOpen(board.boardId), // 클릭 이벤트 시 해당 카드의 index를 전달
+                          onClick: () => handleOpen(board.boardId),
                         }}
                         authors={[
                           { image: team1, name: "Elena Morison" },
@@ -144,24 +138,45 @@ function Board() {
                     </Grid>
                     ))
                   }
-                    <Dialog
-                          open={open}
-                          close={handleClose}
-                          aria-labelledby="title"
-                          fullWidth
-                          maxWidth={'lg'}
-                        >
-                        <DialogContent sx={{ backgroundColor:"#012654"}} >
-                          <Card>
-                            <VuiBox height="100%" display="flex" flexDirection="column" justifyContent="space-between">
-                              <VuiInput></VuiInput>
-                            </VuiBox>
-                          </Card>
-                          <DialogActions sx={{paddingBottom: 2}}>
-                              <VuiButton variant="gradient" color="error" fullWidth onClick={handleClose}>닫기</VuiButton>
-                          </DialogActions>
-                        </DialogContent>
-                      </Dialog>
+                  <Dialog
+                    open={open}
+                    close={handleClose}
+                    aria-labelledby="title"
+                    fullWidth
+                    maxWidth={'md'}
+                    PaperProps={{
+                      sx: {
+                        minHeight: 700,
+                        borderRadius: 3
+                      }
+                    }}
+                    >
+                  <DialogContent>
+                    <Box>
+                      <Box display="flex" alignItems="center" mb={2}>
+                        <IoChatbubbles size="22px" color="#4318ff" />
+                        <Typography variant="h4" ml={1} fontStyle={{ color: "#4318ff" }}>Community</Typography>
+                      </Box>
+                        <VuiTypography component="label" variant="h5" color="sidenav" fontWeight="medium" >
+                            title
+                        </VuiTypography>
+                          <TextField  variant="filled" size="medium" fullWidth />
+                        <Box sx={{ my: 2, borderBottom: "1px solid #e0e0e0" }} /> 
+                      </Box>
+                      <ThemeProvider theme={myTheme}>
+                        <MUIRichTextEditor
+                          label="Type something here..."
+                          // onSave={save}
+                          inlineToolbar={true}
+                        />
+                      </ThemeProvider>
+                    </DialogContent>
+                    <Box sx={{ my: 2, borderBottom: "1px solid #e0e0e0" }} /> 
+                    <DialogActions>
+                      <VuiButton variant="gradient" color="primary" fullWidth>Post</VuiButton>
+                      <VuiButton variant="gradient" color="secondary" fullWidth onClick={handleClose}>Back</VuiButton>
+                    </DialogActions>
+                </Dialog>
               </Grid>
                 <VuiBox mt={4} display="flex" justifyContent="center">
                   <Pagination
