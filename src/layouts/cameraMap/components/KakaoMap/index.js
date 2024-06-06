cfcb4fbff7f8c82c4e45af61ee5f0cd507f3ca92
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2024-06-06 21:45:24 +0900

    Feat: 카카오맵 기능 구현

--- src/layouts/cameraMap/components/KakaoMap/index.js
+++ src/layouts/cameraMap/components/KakaoMap/index.js
@@ -9,8 +9,10 @@ import VuiBox from "components/VuiBox/index.js";
 import danger from "assets/images/danger.png";
 
 import linearGradient from "assets/theme/functions/linearGradient";
-import { Card, ListItemText, MenuItem, Select } from "@mui/material";
+import { Card, Grid, ListItemText, MenuItem, Select } from "@mui/material";
 import Video from "../../../../assets/videos/annotated_output_2.mp4"
+import { safeMap } from "layouts/cameraMap/data/safeMap.js";
+import { Gauge } from "../Gage/index.js";
 
 
 function KakaoMap({position}) {
@@ -21,22 +23,125 @@ function KakaoMap({position}) {
   const cluseterMap = useRef();
   const overlayMap = useRef();
   const customOverlay = useRef();
+  const [polygons, setPolygons] = useState([]);
+  const [mapLayoutType, setMapLayoutType] = useState(1);
 
+  const [mapName, setMapName] = useState('');
+  const [guage, setGuage] = useState(0);
   
+  let data = geojson.features;
+  let coordinates = []; //좌표 저장 배열
+  let name = ''; //행정구 이름
+  let polygonsVar = [];
+
+  const findValueByKey = (obj, key) => {
+    if (key in obj) {
+      return obj[key];
+    } else {
+      return `Key "${key}" not found in the object.`;
+    }
+  }
+
+
+  const displayArea = (coordinates, name, type) => {
+    let path = [];
+    let points = [];
+    
+    const mapType = safeMap[type-1].safeGrade;
+
+    coordinates[0][0].forEach((coordinate) => {
+      let point = {};
+      point.x = coordinate[1];
+      point.y = coordinate[0];
+      points.push(point);
+      path.push(new window.kakao.maps.LatLng(coordinate[1], coordinate[0]));
+    });
+
+    let grade = findValueByKey(mapType, name.split(' ')[1]);
+    var color = '#fff';
+
+    if(grade === 1){
+      color = '#0000ff'
+    } else if(grade === 2) {
+      color = '#00ACFF';
+    } else if (grade === 3) {
+      color = '#339933';
+    } else if (grade === 4) {
+      color = '#FFA500';
+    } else if(grade === 5) {
+      color = '#FF0000';
+    }
+
+    
+
+    let polygon = new window.kakao.maps.Polygon({ 
+      map: map.current,
+      path: path, // 그려질 다각형의 좌표 배열입니다
+      strokeWeight: 2, // 선의 두께입니다
+      strokeColor: '#004c80', // 선의 색깔입니다
+      strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      strokeStyle: 'solid', // 선의 스타일입니다
+      fillColor: color, // 채우기 색깔입니다
+      fillOpacity: 0.7, // 채우기 불투명도 입니다
+    });
+    polygonsVar.push(polygon);
+    // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경하고 오버레이를 설정합니다
+    window.kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
+      polygon.setOptions({ fillColor: '#0ff' });
+      customOverlay.current.setContent('<div class="area">' + name + '</div>');
+      customOverlay.current.setPosition(mouseEvent.latLng);
+      setGuage(grade);
+      setMapName(name);
+      customOverlay.current.setMap(map.current);
+    });
+
+    // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다
+    window.kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
+      if (customOverlay.current.getMap()) {
+        customOverlay.current.setPosition(mouseEvent.latLng);
+      }
+    });
+
+    // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래 색으로 변경하고 오버레이를 숨깁니다
+    window.kakao.maps.event.addListener(polygon, 'mouseout', function () {
+      customOverlay.current.setMap(null);
+      polygon.setOptions({ fillColor: color });
+    });
+
+    setPolygons(polygonsVar);
+  }
+  // 지도에 표시되어 있는 모든 원과 반경정보를 표시하는 선, 커스텀 오버레이를 지도에서 제거합니다
+  const  removePolygon = () => {         
+    polygons.forEach((polygon) => {
+      polygon.setOptions({ fillColor: null });
+      polygon.setMap(null);
+    });     
+     // polygons 배열 초기화
+  }
+
+  const handleMapLayoutChange = (e) => {
+    setMapLayoutType(e.target.value);
+    removePolygon();
+    polygonsVar = []; 
+
+    data.forEach((val) => {
+      coordinates = val.geometry.coordinates;
+      name = val.properties.adm_nm;
+      displayArea(coordinates, name, e.target.value);
+    });
+
+    setPolygons(polygonsVar); 
+  };
 
   
 
   useEffect(() => {
-    let data = geojson.features;
-    let coordinates = []; //좌표 저장 배열
-    let name = ''; //행정구 이름
-    let polygons = [];
-
-
     const script = document.createElement("script");
     script.async = true;
     script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${apiKey}&libraries=services,clusterer,drawing&autoload=false`;
     document.head.appendChild(script);
+
+    
   
     script.addEventListener("load", () => {
       window.kakao.maps.load(() => {
@@ -82,6 +187,7 @@ function KakaoMap({position}) {
         '        <video autoplay loop muted type="video/mp4" controls style="width: 100%; height: auto;">' +
         `          <source src=${Video} type="video/mp4">` +
         '        </video>' +
+        `    <div class="text btn">카메라 추가</div>` + 
         '    </div>' +
         '</div>';
 
@@ -101,56 +207,13 @@ function KakaoMap({position}) {
 
 
         customOverlay.current = new window.kakao.maps.CustomOverlay({});
-        const displayArea = (coordinates, name) => {
-          let path = [];
-          let points = [];
-    
-          coordinates[0][0].forEach((coordinate) => {
-            let point = {};
-            point.x = coordinate[1];
-            point.y = coordinate[0];
-            points.push(point);
-            path.push(new window.kakao.maps.LatLng(coordinate[1], coordinate[0]));
-          });
-
-          let polygon = new window.kakao.maps.Polygon({ 
-            map: map.current,
-            path: path, // 그려질 다각형의 좌표 배열입니다
-            strokeWeight: 2, // 선의 두께입니다
-            strokeColor: '#004c80', // 선의 색깔입니다
-            strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: 'solid', // 선의 스타일입니다
-            fillColor: '#fff', // 채우기 색깔입니다
-            fillOpacity: 0.7, // 채우기 불투명도 입니다
-          });
-          polygons.push(polygon);
-          // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경하고 오버레이를 설정합니다
-          window.kakao.maps.event.addListener(polygon, 'mouseover', function (mouseEvent) {
-            polygon.setOptions({ fillColor: '#09f' });
-            customOverlay.current.setContent('<div class="area">' + name + '</div>');
-            customOverlay.current.setPosition(mouseEvent.latLng);
-            customOverlay.current.setMap(map.current);
-          });
-
-          // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다
-          window.kakao.maps.event.addListener(polygon, 'mousemove', function (mouseEvent) {
-            if (customOverlay.current.getMap()) {
-              customOverlay.current.setPosition(mouseEvent.latLng);
-            }
-          });
-
-          // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래 색으로 변경하고 오버레이를 숨깁니다
-          window.kakao.maps.event.addListener(polygon, 'mouseout', function () {
-            customOverlay.current.setMap(null);
-            polygon.setOptions({ fillColor: '#fff' });
-          });
-        }
+        
         
         data.forEach((val) => {
           coordinates = val.geometry.coordinates;
           name = val.properties.adm_nm;
     
-          displayArea(coordinates, name);
+          displayArea(coordinates, name, 1);
         });
       });
     });
@@ -191,64 +254,76 @@ function KakaoMap({position}) {
   return (
     <>
         <VuiBox sx={{backgroundColor: "#030c1d", borderRadius:"15px", width:"110%"}} mb={1}> 
-          <VuiBox
-            display="flex"
-          >
-            <VuiBox m={4} p={1}>
-              <VuiSwitch color="info" id="chkUseDistrict" onClick={setOverlayMapTypeId} />
-              <VuiTypography variant="button" fontWeight="regular" color="text">
-                지적편집도 정보 보기
-              </VuiTypography>
-            </VuiBox>
-            <VuiBox m={4} p={1}>
-              <VuiSwitch color="info" id="chkTerrain" onClick={setOverlayMapTypeId} />
-              <VuiTypography variant="button" fontWeight="regular" color="text">
-                지형정보 보기 
-              </VuiTypography>
-            </VuiBox>
-            <VuiBox m={4} p={1}>
-              <VuiSwitch color="info" id="chkTraffic" onClick={setOverlayMapTypeId} />
-              <VuiTypography variant="button" fontWeight="regular" color="text">
-                교통정보 보기       
-              </VuiTypography>
-            </VuiBox>
-            <VuiBox m={4} p={1}>
-              <VuiSwitch color="info" id="chkBicycle" onClick={setOverlayMapTypeId} />
-              <VuiTypography variant="button" fontWeight="regular" color="text">
-                자전거도로 정보 보기
-              </VuiTypography>
-            </VuiBox>
-          </VuiBox>
-          <VuiBox width="20%" pl={5}>
-            <VuiTypography variant="button" fontWeight="regular" color="text">
-              ✅  생활 안전 지도 선택
-            </VuiTypography>
-            <Select
-              id="demo-checkbox"
-              // value={modelType}
-              // onChange={modelhandleChange}
-              // MenuProps={MenuProps}
-              p={2}
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <VuiBox
+              display="flex"
             >
-              <MenuItem m={1}>
-                <ListItemText>범죄발생 지도 보기</ListItemText>
-              </MenuItem>
-              <MenuItem>
-                <ListItemText>교통사고 발생 지도 보기</ListItemText>
-              </MenuItem>
-              <MenuItem>
-                <ListItemText>화재발생 지도 보기</ListItemText>
-              </MenuItem>
-              <MenuItem>
-                <ListItemText>생활안전 지도 보기</ListItemText>
-              </MenuItem>
-            </Select>
-          </VuiBox>
-          <VuiBox p={5}>
-            <VuiTypography variant="h4" fontWeight="bold" color="text">
-              ⚠️ 표시는 CCTV 취약 지점입니다. 어서빨리 CCTV를 설치해 주세요!!!
-            </VuiTypography>
-          </VuiBox>
+              <VuiBox m={1} p={1}>
+                <VuiSwitch color="info" id="chkUseDistrict" onClick={setOverlayMapTypeId} />
+                <VuiTypography variant="button" fontWeight="regular" color="text">
+                  지적편집도 정보 보기
+                </VuiTypography>
+              </VuiBox>
+              <VuiBox m={1} p={1}>
+                <VuiSwitch color="info" id="chkTerrain" onClick={setOverlayMapTypeId} />
+                <VuiTypography variant="button" fontWeight="regular" color="text">
+                  지형정보 보기 
+                </VuiTypography>
+              </VuiBox>
+              <VuiBox m={1} p={1}>
+                <VuiSwitch color="info" id="chkTraffic" onClick={setOverlayMapTypeId} />
+                <VuiTypography variant="button" fontWeight="regular" color="text">
+                  교통정보 보기       
+                </VuiTypography>
+              </VuiBox>
+              <VuiBox m={1} p={1}>
+                <VuiSwitch color="info" id="chkBicycle" onClick={setOverlayMapTypeId} />
+                <VuiTypography variant="button" fontWeight="regular" color="text">
+                  자전거도로 정보 보기
+                </VuiTypography>
+              </VuiBox>
+            </VuiBox>
+            <VuiBox width="50%" pl={5}>
+              <VuiTypography variant="button" fontWeight="regular" color="text">
+                ✅  생활 안전 지도 선택
+              </VuiTypography>
+                  <Select
+                    id="demo-checkbox"
+                    value={mapLayoutType}
+                    onChange={handleMapLayoutChange}
+                    // MenuProps={MenuProps}
+                    p={2}
+                  >
+                    <MenuItem m={1} value={1}>
+                      <ListItemText>범죄발생 지도 보기</ListItemText>
+                    </MenuItem>
+                    <MenuItem value={2}>
+                      <ListItemText>교통사고 발생 지도 보기</ListItemText>
+                    </MenuItem>
+                    <MenuItem value={3}>
+                      <ListItemText>화재발생 지도 보기</ListItemText>
+                    </MenuItem>
+                    <MenuItem value={4}>
+                      <ListItemText>생활안전 지도 보기</ListItemText>
+                    </MenuItem>
+                  </Select>
+                </VuiBox>
+                <VuiBox py={2} px={5}>
+                  <VuiTypography variant="h4" fontWeight="bold" color="text">
+                    ⚠️ 표시는 CCTV 취약 지점입니다. CCTV를 설치가 필요한 구역입니다.
+                  </VuiTypography>
+                </VuiBox>
+                </Grid>
+                <Grid item xs={5}>
+                  <VuiBox>
+                    <Gauge guage={guage}/>
+                  </VuiBox>
+                  <VuiTypography variant="h6" fontWeight="bold" color="text">
+                  {mapName} 지역 등급은 현재 {guage} 등급입니다. <span style={{fontSize:"10px"}}>등급은 (0~5) 로 높을 수록 위험합니다.</span>
+                  </VuiTypography>
+                </Grid>
+          </Grid>
         </VuiBox>
       <div id="map" style={{ width: "110%", height: "100%", borderRadius:"20px" }}>
       </div>
